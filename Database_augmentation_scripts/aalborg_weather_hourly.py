445018a8b18e0ea078d76faf76b5576ab3ad968d
mateimaat <164197387+mateimaat@users.noreply.github.com>
2025-10-13 14:20:04 +0200

    [DOC] Added documentation for the weather augmentation (#28)

--- Database_augmentation_scripts/aalborg_weather_hourly.py
+++ Database_augmentation_scripts/aalborg_weather_hourly.py
@@ -1,16 +1,32 @@
+"""
+Retrieve and export the historical weather data for a specific locatiom, in this case AAU Build location.
+
+This script is using the Meteostat API to download hourly temperature data, between two time periods. The data is saved as a CSV file for further analysis.
+
+Requirements:
+    meteostat (install using pip install meteostat)
+
+Modules:
+    datetime: Used for defining the time range.
+    meteostat: Provides weather data retrieval functionality.
+
+Output:
+    output_file: CSV with hourly weather data with timestamps.
+"""
+
 from datetime import datetime
 from meteostat import Point, Hourly
 import time
-start_time = time.time()
 
-location = Point(57.014768, 9.974116)  # AAU Build
+# change as needed
+output_file = "aalborg_weather_hourly_test2.csv"
+
+start_time = time.time()
+location = Point(57.014768, 9.974116)
 start = datetime(2024, 9, 7)
 end = datetime(2025, 5, 11)
-# we specify the location, start and end date of the dataset
 data = Hourly(location, start, end)
 data = data.fetch()
-# we extract the weather data hourly ( we cannot extract it minute-by-minute )
 temp_data = data[['temp']]
-# we want to extract only the temperature from the API
-temp_data.to_csv("aalborg_weather_hourly_test2.csv")
+temp_data.to_csv(output_file)
 print("--- %s seconds ---" % (time.time() - start_time))
