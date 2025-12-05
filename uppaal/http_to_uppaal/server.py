013dfef12a940a1466e1e6e7e2336ddd5ea8e335
beltranaceves <beltranaceves@gmail.com>
2025-12-05 11:10:11 +0100

    New uppaal elements and probabilistic checking

--- uppaal/http_to_uppaal/server.py
+++ uppaal/http_to_uppaal/server.py
@@ -180,7 +180,7 @@ class ModelWrapper:
 
         if not latent_dim or latent_dim <= 0:
             # As a last resort choose a reasonable default
-            latent_dim = 16
+            latent_dim = 32
             print(f"Using default latent_dim={latent_dim}")
 
         # Instantiate model and load weights
